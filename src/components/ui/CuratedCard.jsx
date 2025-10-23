5e1091614017fc10fd7d44f9413889521b79d01a
Akshi1277 <verstapenalfred@gmail.com>
2025-10-23 19:53:01 +0530

    update CuratedCard.jsx

--- src/components/ui/CuratedCard.jsx
+++ src/components/ui/CuratedCard.jsx
@@ -20,12 +20,13 @@ export default function CuratedCard({ item, darkMode }) {
           <p className="text-white/90 text-xs md:text-sm line-clamp-2">{item.description}</p>
         </div>
       </div>
-      <div className="p-2 md:p-3">
-        <button className="w-full cursor-pointer bg-emerald-600 hover:bg-emerald-700 text-white py-2 md:py-2.5 rounded-lg text-xs md:text-sm font-medium transition-colors flex items-center justify-center gap-1.5">
-          <span>Support Now</span>
-          <ArrowRight className="w-3 h-3 md:w-4 md:h-4" />
-        </button>
-      </div>
+    <div className="mt-auto">
+  <button className="w-full rounded-b-2xl bg-emerald-600 hover:bg-emerald-700 text-white py-3 md:py-3.5 text-sm md:text-base font-medium transition-colors flex items-center justify-center gap-1.5">
+    <span>Support Now</span>
+    <ArrowRight className="w-4 h-4" />
+  </button>
+</div>
+
     </div>
   );
 }
