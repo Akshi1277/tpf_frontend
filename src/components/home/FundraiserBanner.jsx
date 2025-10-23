d86c88f1ab307ef21ab10fe30c2a1da47b7c450a
aquib2502 <aquibhingwala@gmail.com>
2025-10-23 23:37:11 +0530

    --changed the zakaat icon clarity and size in navbar -- re aligned the footer and rearraged the sections of about us ways to jnnah and get involve -- updated the size of impact stories section amde it smaller to fir window and changed text -- changed image and text of last fundraising card

--- src/components/home/FundraiserBanner.jsx
+++ src/components/home/FundraiserBanner.jsx
@@ -7,7 +7,7 @@ export default function StartFundraiserBanner({ darkMode }) {
         <div className="relative overflow-hidden rounded-2xl">
           <div className="absolute inset-0 bg-gradient-to-r from-black/60 to-black/30"></div>
           <img
-            src="https://images.unsplash.com/photo-1547069545-e88faa5cac9d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fFJlYWNoJTIwbWlsbGlvbnMlMjBvZiUyMGRvbm9ycyUyMHdvcmxkd2lkZS4lMjBUdXJuJTIweW91ciUyMGNhdXNlJTIwaW50byUyMGltcGFjdCUyMHRvZGF5LnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&q=60&w=600"
+            src="https://images.unsplash.com/photo-1576381394626-53b3d2d48145?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=500"
             alt="Start Your Fundraiser"
             className="h-64 md:h-96 w-full object-cover object-[50%_75%]"
           />
@@ -17,10 +17,10 @@ export default function StartFundraiserBanner({ darkMode }) {
                 Start your fundraiser in minutes
               </h3>
               <p className="text-sm md:text-base text-white/90 mb-5">
-                No platform fees. Reach millions of donors worldwide. Turn your cause into impact today.
+                No platform fees. Reach millions of generous hearts across the Ummah. Turn your cause into lasting ṣadaqah jāriyah — a reward that continues beyond this world.
               </p>
               <button className="px-6 py-3 cursor-pointer bg-emerald-600 hover:bg-emerald-700 text-white rounded-lg font-medium text-lg transition-colors">
-                Create Campaign Now
+                Create Fundraiser Now
               </button>
             </div>
           </div>
