1a8418173c69d3aac83b09f3ec533b4402931496
Akshi1277 <verstapenalfred@gmail.com>
2025-11-06 15:39:38 +0530

    update page.jsx

--- src/app/userprofile/page.jsx
+++ src/app/userprofile/page.jsx
@@ -1,28 +1,6 @@
 "use client"
-import ProfilePage from '@/components/userprofile/UserProfile';
-import React from 'react'
-import { useEffect, useState } from 'react';
 
-const page = () => {
-      const [darkMode, setDarkMode] = useState(false);
-    
-      useEffect(() => {
-        const savedMode = localStorage.getItem('darkMode');
-        setDarkMode(savedMode === 'true');
-      }, []);
-    
-      useEffect(() => {
-        localStorage.setItem('darkMode', darkMode);
-      }, [darkMode]);
-
-  return (
-    <div>
-        
-        <ProfilePage darkMode={darkMode} />
-
-        </div>
-   
-  )
+import ProfilePage from "@/components/userprofile/UserProfile"
+export default function Page() {
+  return <ProfilePage/>
 }
-
-export default page
