67ec257204585156dd2926349ecb67557d492484
aquib2502 <aquibhingwala@gmail.com>
2025-11-10 17:17:51 +0530

    changed file name again to contactus

--- src/app/contactus/page.jsx
+++ src/app/contactus/page.jsx
@@ -1,5 +1,6 @@
 "use client"
-import ContactPage from '@/components/contactus/ContactUs';
+import ContactPage from '@/components/contactus/contactus';
+
 import Footer from '@/components/layout/Footer';
 import Navbar from '@/components/layout/Navbar';
 import React from 'react'
