03bd0ce8917bc7ddd9fb7feab508dd2a342a8b2d
Akshi1277 <verstapenalfred@gmail.com>
2026-01-30 16:53:11 +0530

    changes for ticket and queries

--- src/utils/slices/apiSlice.js
+++ src/utils/slices/apiSlice.js
@@ -28,7 +28,7 @@ const baseQueryWithAutoLogout = async (args, api, extraOptions) => {
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: baseQueryWithAutoLogout,
-  tagTypes: ["CMS", "Campaign", "User", "Wishlist", "RecentTransactions", "PeopleHelped", "Leaderboard", "Comments", "MyDonations", "MyApplications", "Vouchers", "Blog"],
+  tagTypes: ["CMS", "Campaign", "User", "Wishlist", "RecentTransactions", "PeopleHelped", "Leaderboard", "Comments", "MyDonations", "MyApplications", "Vouchers", "Blog","FAQ"],
   endpoints: (builder) => ({
     getHijriDate: builder.query({
       async queryFn(date) {
